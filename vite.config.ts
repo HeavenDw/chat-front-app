e8d8e7717777ed89c8a87cd88b2aa0f975d54986
Konstantin Otinov <kotinov@dar.io>
2025-04-19 20:51:47 +0300

    changed vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
-import react from '@vitejs/plugin-react';
+import { babel } from '@rollup/plugin-babel';
 import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    babel({ extensions: ['.ts', '.tsx'], babelHelpers: 'bundled', skipPreflightCheck: true }),
+  ],
   resolve: {
     alias: [
       {
